dfaef626de66a5385f813f7828c2f8ffa9f1eaa9
DenisIng <denis.kis@ingsoftware.com>
2023-08-30 19:00:53 +0200

    Initial push

---
+++ My Store/Assets/ProjectAssets/Scripts/Enums.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enums
+{
+    
+}
+
+public enum ItemType
+{
+    Clothing,
+    Drink
+}
+
+public enum ClothingType
+{
+
+}
---
+++ My Store/Assets/ProjectAssets/Scripts/ItemData.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class ItemData
+{
+    public string Name;
+    public string Description;
+    public float Weight;
+    public int SellPrice;
+    public int BuyPrice;
+}
